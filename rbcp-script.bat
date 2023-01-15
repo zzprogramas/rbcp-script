904cae41af5104ca7a7765a0dcb0639af4e39e81
Itati <zzjuegos003@gmail.com>
2023-01-15 20:12:06 +0100

    selecion automatica de usuario con nueva ubicacion, ruta de destino con seleccion automatica del año que este corriendo, se simplifica los archivos temporales por defecto en la variante de archivos temporales y se agrega una log de errores en en la rura de donde se recoge la ruta el txt que se se va hacer la copia

--- rbcp-script.bat
+++ rbcp-script.bat
@@ -1,4 +1,4 @@
-for /f "delims=" %%m in (C:\Users\usuario\Desktop\makebackup\filestobackup.txt) do (
-	if not exist "H:\backup_rbcpy2022%%~pm" mkdir "H:\backup_rbcpy2022%%~pm"
-	robocopy "%%m" "H:\backup_rbcpy2022%%~npxm" /E /DCOPY:DAT /R:1 /W:2 /NP /LOG+:"C:\temp01\robocopylog_%%~nzxm.txt"
+for /f "delims=" %%m in (%USERPROFILE%\Documents\makebackup\filestobackup.txt) do (
+	if not exist "H:\backup_rbcpy%DATE:~6,4%%%~pm" mkdir "H:\backup_rbcpy%DATE:~6,4%%~pm"
+	robocopy "%%m" "H:\backup_rbcpy%DATE:~6,4%%~npxm" /E /DCOPY:DAT /R:1 /W:2 /NP /LOG+:"%TEMP%\robocopylog_%%~nzxm.txt" /UNILOG+:%~dp0error-backup%%~nzxm.txt
 )
